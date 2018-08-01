74ce99d5ddffc78e2a1d3307d985ffc82ff856f1
YousefAlyami911 <41991944+YousefAlyami911@users.noreply.github.com>
2018-08-02 00:38:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = "y";
+const prefix = 'y'
+
 client.on('ready', () => {
-    console.log('I am ready!');
-});
-client.on('message', message => {
-    if (message.content === 'zg') {
-        message.reply('pong');
-      }
-});
+  console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 
-
-
-client.login(process.env.BOT_TOKEN);  
-
+client.login(process.env.BOT_TOKEN);
