821ea8b464160b8de361a43be8f8f93d43d0892f
YousefAlyami911 <41991944+YousefAlyami911@users.noreply.github.com>
2018-08-01 22:22:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,64 +44,7 @@ var download = function(uri, filename, callback) {
         console.log('content-length:', res.headers['content-length']);
 
         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-};
-
-client.on('message', function(message) {
-    const member = message.member;
-    const mess = message.content.toLowerCase();
-    const args = message.content.split(' ').slice(1).join(' ');
-
-    if (mess.startsWith(prefix + 'p')) {
-        if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **__??? ?? ???? ?? ??? ????__**');
-        // if user is not insert the URL or song title
-        if (args.length == 0) {
-            let play_info = new Discord.RichEmbed()
-                .setAuthor(client.user.username, client.user.avatarURL)
-                .setFooter('??? ??????: ' + message.author.tag)
-                .setDescription('**?? ?????? ???? ?? ??? ???????**')
-            message.channel.sendEmbed(play_info)
-            return;
-        }
-        if (queue.length > 0 || isPlaying) {
-            getID(args, function(id) {
-                add_to_queue(id);
-                fetchVideoInfo(id, function(err, videoInfo) {
-                    if (err) throw new Error(err);
-                    let play_info = new Discord.RichEmbed()
-                        .setAuthor(client.user.username, client.user.avatarURL)
-                        .addField('??? ???????????? ?????? ????????', `**
-                          ${videoInfo.title}
-                          **`)
-                        .setColor("#a637f9")
-                        .setFooter('|| ' + message.author.tag)
-                        .setThumbnail(videoInfo.thumbnailUrl)
-                    message.channel.sendEmbed(play_info);
-                    queueNames.push(videoInfo.title);
-                    now_playing.push(videoInfo.title);
-
-                });
-            });
-        }
-        else {
-
-            isPlaying = true;
-            getID(args, function(id) {
-                queue.push('placeholder');
-                playMusic(id, message);
-                fetchVideoInfo(id, function(err, videoInfo) {
-                    if (err) throw new Error(err);
-                    let play_info = new Discord.RichEmbed()
-                        .setAuthor(client.user.username, client.user.avatarURL)
-                        .addField('__**?? ??????? ?**__', `**${videoInfo.title}
-                              **`)
-                        .setColor("RANDOM")
-                        .addField(`??????`, message.author.username)
-                        .setThumbnail(videoInfo.thumbnailUrl)
-                });
-            });
-        }
-    }
+ });
     else if (mess.startsWith(prefix + 's')) {
         if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **__??? ?? ???? ?? ??? ????__**');
         message.channel.send('`?`').then(() => {
@@ -223,62 +166,7 @@ function search_video(query, cb) {
 function isYoutube(str) {
     return str.toLowerCase().indexOf('youtube.com') > -1;
 }
- client.on('message', message => {
-     if (message.content === prefix +"help") {
-    const embed = new Discord.RichEmbed()
-     .setColor("RANDOM")
-     .addField(`Zyad,aLmutairi commands:
 
-+about - shows info about the bot
-+ping - checks the bot's latency
-
-  Music:
-
-+play - shows the song that is currently playing
-+play <title|URL|subcommand> - plays the provided song
-+queue [pagenum] - shows the current queue
-+???? <title|URL|subcommand> - plays the provided song
-+skip - votes to skip the current song
-
-  DJ:
-+ok <title|URL|subcommand> - plays the provided song
-+skip - skips the current song
-+pause - pauses the current song
-+skipt <position> - skips to the specified song
-+stop - stops the current song and clears the queue
-+vol [0-150] - sets or shows volume
-
-For additional help,  `)
-
-      message.channel.send({embed});
-     }
-    });
-	client.on('message', message => {
-    if (message.content.startsWith("habout")) {
-      message.channel.send({
- embed: new Discord.RichEmbed() 
-    .setColor('RED')
-    .addField('**??????? ????????? ??**', `${(process.memoryUsage().rss / 1000000).toFixed()}MB`, true)
-         .addField('**???? ?????????**' , `${Date.now() - message.createdTimestamp}` + ' ms')
-        .addField('**??? ????????**', timeCon(process.uptime()), true)
-        .addField('**??????? ?????????**', `${(process.cpuUsage().rss / 10000).toFixed()}%`, true)
-     })
-    }
-  });
-  client.on('message', message => {
-                                if(!message.channel.guild) return;
-                        if (message.content.startsWith('hping')) {
-                            if(!message.channel.guild) return;
-                            var msg = `${Date.now() - message.createdTimestamp}`
-                            var api = `${Math.round(client.ping)}`
-                            if (message.author.bot) return;
-                        let embed = new Discord.RichEmbed()
-                        .setAuthor(message.author.username,message.author.avatarURL)
-                        .setColor('RANDOM')
-                        .addField('**Time Taken:**',msg + " ms ?? ")
-                        .addField('**WebSocket:**',api + " ms ?? ")
-         message.channel.send({embed:embed});
-  });
 
 
 client.login(process.env.BOT_TOKEN);
